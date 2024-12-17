23665a785b7096542bbf7da9a5f5bb92c018439f
jknohr <johann@nohr.com>
2024-12-18 02:40:20 +0700

    Initial deployment

---
+++ assets/js/dehydrate.js
@@ -0,0 +1,29 @@
+// Dehydrate data into a global variable
+window.DEHYDRATED_DATA = {
+    identity: {
+        name: 'Neural Reef',
+        image: 'assets/images/neural-reef-logo.png'
+    },
+    NavBar: {
+        name: 'Neural Reef',
+        image: 'assets/images/neural-reef-logo.png',
+        links: ['home', 'about', 'modules', 'contact']
+    },
+    contact: {
+        cards: [
+            {
+                title: "Email",
+                description: "secure@neuralreef.ai"
+            },
+            {
+                title: "GitHub",
+                description: "github.com/neural-reef"
+            },
+            {
+                title: "Network",
+                description: "Join our decentralized network"
+            }
+        ]
+    },
+    // ... other data
+}; 
