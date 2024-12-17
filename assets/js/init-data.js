86817cec5a0f9a94be6959d3950f68ae8443ad14
jknohr <johann@nohr.com>
2024-12-18 03:56:48 +0700

    Fix data service and components

--- assets/js/init-data.js
+++
@@ -1,9 +0,0 @@
-// Initialize data structure
-window.data = window.data || {
-    identity: {},
-    quote: {},
-    NavBar: { links: [] }
-};
-
-// Log initialization
-console.log('Data structure initialized:', window.data); 
